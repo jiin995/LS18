ba011fb095ea5b059b6fbd78fe56f2a70443a031
jiin995 <gabrieleprevitera@gmail.com>
2018-04-21 00:17:45 +0200

    funziona si deve solo prevedere il cambio password

--- crisis/dockerize.sh
+++ crisis/dockerize.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-DB_NAME="crisis"
+#DB_NAME="crisis"
 DB_LOCAL_USER="root"
 DB_LOCAL_PASS="toor"
+
+DB_CONTAINER_PASS="torro!234"
+DB_CONTAINER_NEW_PASS="torro!234"
+DB_CONTAINER_CRISIS_OLD_PASS="torro!234"
+DB_CONTAINER_CRISIS_NEW_PASS="torro!234"
 #della nuova istanza
-DB_WP_USER="myword"
-DB_WP_PASS="mypass"
-DB_NEW_PASS="i93dd3342248"
-WP_IP=""
 
 PWD=$(pwd)
 C_DIR_NAME=$(basename "$PWD")
@@ -20,45 +21,41 @@ PHP_CONTAINER="$C_DIR_NAME""_php_1"
 
 #mysqladmin -u root --password=$DB_PASS password $DB_NEW_PASS
 
-#----------------------------------------------
-#parte di aggiornamento compose
-
-#--------------------------
-#!/bin/sh
 
 service apache2 stop
 chmod +x set_db.sh
-
-cp docker-compose.yml docker-compose.yml_bak
-
-sed -i -e "s/MYSQL_USER: wordpress/MYSQL_USER: $DB_WP_USER/" docker-compose.yml
-sed -i -e "s/MYSQL_PASSWORD: wordpress/MYSQL_PASSWORD: $DB_WP_PASS/" docker-compose.yml
-sed -i -e "s/MYSQL_ROOT_PASSWORD: somewordpress/MYSQL_ROOT_PASSWORD: $DB_NEW_PASS/" docker-compose.yml
-sed -i -e "s/WORDPRESS_DB_USER: wordpress/WORDPRESS_DB_USER: $DB_WP_USER/" docker-compose.yml
-sed -i -e "s/WORDPRESS_DB_PASSWORD: wordpress/WORDPRESS_DB_PASSWORD: $DB_WP_PASS/" docker-compose.yml
 
 cp -dr /etc/apache2/ssl/ myphp/
 cp -dr /etc/apache2/sites-available/ myphp/
 cp -dr /etc/apache2/sites-enabled/  myphp/
 
+#copio la configurazione di mysql
+mkdir /var/lib/docker/volumes/crisis_dbdata/
+mkdir /var/lib/docker/volumes/crisis_dbdata/_data
+cp -rafd ../../Downloads/crisis.mil04.ex/mysqlvarlibfolder/mysql/* /var/lib/docker/volumes/crisis_dbdata/_data
+#cp -rafd /var/lib/mysql/* /var/lib/docker/volumes/crisis_dbdata/_data
+
+
+
 docker-compose build
 docker-compose up -d
-sleep 30
+sleep 5
+
 #preparo il container per il restore del dump e faccio il restore
 docker cp set_db.sh $DB_CONTAINER:/home
 docker cp dump.sql $DB_CONTAINER:/home
-docker exec $DB_CONTAINER ./home/set_db.sh root $DB_NEW_PASS $DB_NAME
+docker exec $DB_CONTAINER ./home/set_db.sh root $DB_CONTAINER_PASS $DB_CONTAINER_NEW_PASS $DB_CONTAINER_CRISIS_NEW_PASS
 
 rm -dr myphp/ssl
 rm -dr myphp/sites-available
 rm -dr myphp/sites-enabled
 
-#cp -dr /etc/apache2/sites-available/* /var/lib/docker/volumes/wp_apache-available/_data/
-#cp -dr /etc/apache2/sites-enabled/*  /var/lib/docker/volumes/wp_apache-enabled/_data/
-#cp -dr  /var/lib/docker/volumes/wp_apache-ssl/_data
 
 cp -aRdfr /var/www/html/* /var/lib/docker/volumes/crisis_http-data/_data/
 
 sed -i -e "s/localhost/db/" /var/lib/docker/volumes/crisis_http-data/_data/connect.php
+docker exec $PHP_CONTAINER docker-php-ext-install mysqli
+docker exec $PHP_CONTAINER docker-php-ext-enable mysqli
+
 docker exec $PHP_CONTAINER service apache2 reload
 #mv docker-compose.yml_bak docker-compose.yml
--- crisis/set_db.sh
+++ crisis/set_db.sh
@@ -12,9 +12,16 @@ cp /etc/mysql/mysql.cnf  /etc/mysql/mysql.cnf_bak
   echo "user=$1" >> /etc/mysql/mysql.cnf
   echo "password=$2" >> /etc/mysql/mysql.cnf
 
-mysql -e "drop database $3;"
-mysql -e "create database $3;"
-mysql --database=$3 < /home/dump.sql
+#mysql -e "drop database $4;"
+#mysql -e "create database $4;"
+#mysql --database=$3 < /home/dump.sql
+
+mysql -e 'create user "crisis"@"%" identified by "ciccio";'
+mysql -e "grant all privileges on *.* to 'crisis'@'%';"
+mysql -e "set password for 'crisis'@'%'=PASSWORD('$4');"
+
+
+#mysqladmin -u crisis --password='torro!234' password "'$4'"
 
 rm /home/dump.sql
 mv /etc/mysql/mysql.cnf_bak  /etc/mysql/mysql.cnf
